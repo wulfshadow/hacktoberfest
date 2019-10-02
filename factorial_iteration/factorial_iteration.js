13d6423a0cf6935a1e81c9e4e6e601dcd4e25b17
Ezra Lazuardy <me@ezralazuardy.com>
2019-10-02 23:24:06 +0700

    Create factorial_iteration.js

---
+++ factorial_iteration/factorial_iteration.js
@@ -0,0 +1,12 @@
+let number = 4;
+
+console.log(factorial(number));
+
+function factorial(n) {
+    if(n == 0) return 1;
+    product = 1;
+    for(i = 1; i <= n; i++) {
+        product *= i;
+    }
+    return product;
+}
