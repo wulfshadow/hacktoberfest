b1ef6a067882a4a26386ec7210d0eefd7890ac22
Ezra Lazuardy <me@ezralazuardy.com>
2019-10-02 23:33:03 +0700

    Update factorial_iteration.js

--- factorial_iteration/factorial_iteration.js
+++ factorial_iteration/factorial_iteration.js
@@ -5,8 +5,6 @@ console.log(factorial(number));
 function factorial(n) {
     if(n == 0) return 1;
     product = 1;
-    for(i = 1; i <= n; i++) {
-        product *= i;
-    }
+    for(i = 1; i <= n; i++)  product *= i;
     return product;
 }
